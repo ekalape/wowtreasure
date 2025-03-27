a03a55e3603dea3d70f9bdfd2a665f17564b0a8a
Ekalape <ekalape@outlook.it>
2025-03-27 20:44:00 +0100

    fix: add a visible message if array is empty, adjust loading styles

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Loading() {
     console.log("Loading is called")
     return (
-        <div className='fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-background opacity-80 blur z-50'>
+        <div className='fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-background opacity-80 backdrop-blur-md z-50'>
 
             <Loader />
         </div>
--- src/app/stats/layout.tsx
+++ src/app/stats/layout.tsx
@@ -1,36 +1,33 @@
-
-import TwoDateChooser from '@/components/DateChooser/TwoDatesChooser'
-import { NuqsAdapter } from 'nuqs/adapters/next'
-import React from 'react'
+import TwoDateChooser from '@/components/DateChooser/TwoDatesChooser';
+import { NuqsAdapter } from 'nuqs/adapters/next';
+import React from 'react';
 
 type LayoutProps = {
-    children: React.ReactNode
-    CalendarChooser: React.ReactNode
-    Details: React.ReactNode
-}
+  children: React.ReactNode;
+  CalendarChooser: React.ReactNode;
+  Details: React.ReactNode;
+};
 
 export default function Layout({ children, CalendarChooser, Details }: LayoutProps) {
+  return (
+    <NuqsAdapter>
+      <div className='w-full h-full grid grid-cols-[1fr_2fr] gap-3 justify-items-center m-auto justify-center'>
+        <section className='w-1/2 border-2 border-background_alt p-4 rounded-lg col-span-2 items-center flex justify-center'>
+          <TwoDateChooser />
+        </section>
 
-    return (
-        <NuqsAdapter>
-            <div className='w-full h-full grid grid-cols-[1fr_2fr] gap-3 justify-items-center m-auto justify-center'>
+        <section className='w-full border-2 border-background_alt p-4 rounded-lg flex flex-col items-center justify-center'>
+          {CalendarChooser}
+        </section>
 
-                <section className='w-1/2 border-2 border-background_alt p-4 rounded-lg col-span-2 items-center flex justify-center'>
-                    <TwoDateChooser />
-                </section>
+        <section className='w-full border-2 border-background_alt p-4 rounded-lg flex flex-col justify-start'>
+          {Details}
+        </section>
 
-                <section className='w-full border-2 border-background_alt p-4 rounded-lg flex flex-col items-center justify-center'>
-                    {CalendarChooser}
-                </section>
-
-                <section className='w-full border-2 border-background_alt p-4 rounded-lg flex flex-col justify-start'>
-                    {Details}
-                </section>
-
-                <section className='w-1/2 border-2 flex border-background_alt p-4 rounded-lg col-span-2 justify-center items-center'>
-                    {children}
-                </section>
-            </div>
-        </NuqsAdapter>
-    )
+        <section className='min-w-1/2 w-5/6 border-2 flex border-background_alt p-4 rounded-lg col-span-2 justify-center items-center'>
+          {children}
+        </section>
+      </div>
+    </NuqsAdapter>
+  );
 }
