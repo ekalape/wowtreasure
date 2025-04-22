638bc41430231d800e71edea39c5d17e73871327
Ekalape <ekalape@outlook.it>
2025-04-22 17:14:34 +0200

    feat: add dynamic sorting in stats details

--- src/app/stats/components/StatsDetails.tsx
+++ src/app/stats/components/StatsDetails.tsx
@@ -1,18 +1,19 @@
 'use client';
-
 import { parseAsString, useQueryState } from 'nuqs';
 import { handleProditDataByChar, handleProfitData } from '@/lib/utils/handleProfitData';
 
 import { isSameDay } from 'date-fns';
 import CharCardDataView from '@/components/CharCardDataView/CharCardDataView';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { IChar } from '@/lib/models/char.interface';
 import { transformToDate } from '@/lib/utils/transformDate';
+import Sortsvg from '@/components/Sortsvg';
 
 export default function StatsDetails({ chars }: { chars: IChar[] }) {
   const [from] = useQueryState('from', parseAsString.withOptions({ shallow: false }));
   const [to] = useQueryState('to', parseAsString.withOptions({ shallow: false }));
   const [dayToView] = useQueryState('day', parseAsString.withOptions({ shallow: false }));
+  const [descendentSorting, setDescendentSorting] = useState(true);
 
   const profits = useMemo(() => {
     if (!from || !to) return [];
@@ -20,7 +21,7 @@ export default function StatsDetails({ chars }: { chars: IChar[] }) {
   }, [chars, from, to]);
 
   const profitsByChars = useMemo(() => {
-    return handleProditDataByChar(profits).sort((a, b) => b.rangeProfit - a.rangeProfit);
+    return handleProditDataByChar(profits);
   }, [profits]);
 
   const profitsByDate = useMemo(() => {
@@ -72,24 +73,36 @@ export default function StatsDetails({ chars }: { chars: IChar[] }) {
   return (
     <div className='grid grid-cols-[repeat(2,minmax(350px,50%))] gap-12 items-start'>
       <div className='flex flex-col gap-2'>
-        <h3>
+        <h3 className='flex  gap-3 items-center justify-center'>
           <span>Total Profit: </span>
           <span className='highlighted font-yatra'>
             {profits.reduce((acc, pr) => acc + pr.fullProfit, 0)}
           </span>
+          <button
+            className='w-8 h-8 p-1 opacity-50 hover:opacity-100 duration-200'
+            onClick={() => {
+              setDescendentSorting((prev) => !prev);
+            }}>
+            <Sortsvg color='#FCAACA' />
+          </button>
         </h3>
+
         <div className='flex flex-col gap-2'>
           {profitsByChars.length === 0 && noRangeProfits}
-          {profitsByChars.map((pr) => (
-            <CharCardDataView
-              key={pr.char.charid}
-              id={pr.char.charid}
-              charclass={pr.char.charclass}
-              fraction={pr.char.fraction}>
-              {' '}
-              {pr.char.name} - {pr.rangeProfit}
-            </CharCardDataView>
-          ))}
+          {profitsByChars
+            .sort((a, b) =>
+              descendentSorting ? b.rangeProfit - a.rangeProfit : a.rangeProfit - b.rangeProfit,
+            )
+            .map((pr) => (
+              <CharCardDataView
+                key={pr.char.charid}
+                id={pr.char.charid}
+                charclass={pr.char.charclass}
+                fraction={pr.char.fraction}>
+                {' '}
+                {pr.char.name} - {pr.rangeProfit}
+              </CharCardDataView>
+            ))}
         </div>
       </div>
       {dayToView && (
---
+++ src/components/Sortsvg.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Sortsvg({ color }: { color: string }) {
+  return (
+    <div>
+      <svg viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+        <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
+        <g id='SVGRepo_iconCarrier'>
+          {' '}
+          <path d='M8 0L2 6V7H14V6L8 0Z' fill={color}></path>
+          <path d='M8 16L2 10V9H14V10L8 16Z' fill={color}></path>
+        </g>
+      </svg>
+    </div>
+  );
+}
